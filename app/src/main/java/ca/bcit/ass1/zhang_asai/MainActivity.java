f0be36be375784759ee5286dba7edb79c66e5881
Keishi Asai <isheik808@gmail.com>
2018-10-18 01:36:11 -0700

    Add Country class

--- app/src/main/java/ca/bcit/ass1/zhang_asai/MainActivity.java
+++ app/src/main/java/ca/bcit/ass1/zhang_asai/MainActivity.java
@@ -10,9 +10,11 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog pDialog;
     private ListView lv;
     // URL to get contacts JSON
-    private static String SERVICE_URL = "https://restcountries.eu/rest/v2/all?fields=region";
+    private static String SERVICE_URL = "https://restcountries.eu/rest/v2/all";
 //    private static String FILTERS = "?fields=region;name;capital;population;area;borders;flag";
-    private static String FILTERS = "?fields=region";
+    private static String FILTER_HEADER = "?fields=";
+    private static String FILTERS = "name;capital;region;population;area;borders;flag";
+    private static String REQUEST_URL = SERVICE_URL + FILTER_HEADER + FILTERS;
 //    private ArrayList<Toon> toonList;
 
     // test commit
