e46f0414fb49683e10bb53c75d6f6ae4242fa8b0
Keishi Asai <isheik808@gmail.com>
2018-10-18 00:03:26 -0700

    test commit

--- app/src/main/java/ca/bcit/ass1/zhang_asai/MainActivity.java
+++ app/src/main/java/ca/bcit/ass1/zhang_asai/MainActivity.java
@@ -1,10 +1,21 @@
 package ca.bcit.ass1.zhang_asai;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+    private ListView lv;
+    // URL to get contacts JSON
+    private static String SERVICE_URL = "https://restcountries.eu/rest/v2/all?fields=region";
+//    private static String FILTERS = "?fields=region;name;capital;population;area;borders;flag";
+    private static String FILTERS = "?fields=region";
+//    private ArrayList<Toon> toonList;
 
+    // test commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
